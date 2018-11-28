a109536972b5f4b536d1ad620d3b38d07430aabc
htinney <hunter.tinney@gmail.com>
2018-11-28 11:47:53 -0700

    Program buttons responsive

--- pipelion/lion-gui/dashboard.py
+++ pipelion/lion-gui/dashboard.py
@@ -6,8 +6,10 @@ try:
 	from PySide import QtGui as QtWidgets
 	from PySide import QtGui as QtGui
 	from PySide import QtCore
+	from PySide.QtCore import Slot
 except ImportError:
 	from PySide2 import QtWidgets, QtGui, QtCore
+	from PySide2.QtCore import Slot
 
 import json
 
@@ -28,18 +30,17 @@ class Pipelion:
 		print("Importing program list...")
 		for program in self.config["programs"]:
 			print(program["name"])
-			self.programs[program["name"]] = program
+			self.programs[program["id"]] = program
 
 		# Import a list of deparments in this show
 		self.departments = {}
 		print("Importing department list...")
 		for department in self.config["departments"]:
 			print(department["name"])
-			self.departments[department["name"]] = department
+			self.departments[department["id"]] = department
 
 		# Import settings.lion
 		self.settings = json.loads(open("../settings.lion").read())
-
 
 	def startProgram(self):
 		self.app = QtWidgets.QApplication(sys.argv)
@@ -48,19 +49,25 @@ class Pipelion:
 		self.app.exec_()
 
 class ProgramButton(QtWidgets.QPushButton):
-    def __init__(self, pipelion, name, action):
+    def __init__(self, pipelion, program):
         self.pipelion = pipelion
-        icon = QIcon(config["programs"][name]["icon"])
-        title = config["programs"][name]["title"]
-        super(ProgramButton, self).__init__(self, icon, title)
-        self.clicked.connect(action)
+        self.program = program
+        icon = QtWidgets.QIcon("../" + program["icon"])
+        title = program["name"]
+        super(ProgramButton, self).__init__(icon, title)
 
-class ProgramBar(QtWidgets.QHBoxLayout):
-    def __init__(self, pipelion, program_buttons):
+class ProgramBar(QtWidgets.QButtonGroup):
+    def __init__(self, pipelion, main_window, program_buttons):
         self.pipelion = pipelion
+        self.main_window = main_window
         super(ProgramBar, self).__init__()
         for program_button in program_buttons:
-            self.addWidget(program_button)
+            self.addButton(program_button)
+        self.buttonClicked[int].connect(lambda x: self.handle(x))
+
+    @Slot(int)
+    def handle(self, index):
+        self.main_window.startProgram(self.button(index).program)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, pipelion):
@@ -70,17 +77,35 @@ class MainWindow(QtWidgets.QMainWindow):
 		self.setWindowTitle("Pipelion")
 
     def createPanes(self):
-        self.createToolBar()
+        self.createProgramBar()
         self.createCheckedOutPane()
         self.createMainPane()
         self.createPreviewPane()
 
-    def createToolBar(self):
+    @Slot(int)
+    def startProgram(self, index):
+		print("called it")
+		print(index)
+
+    def startProgram(self, program, file="untitled"):
+        print("Starting " + program["name"])
+        print("Opening " + file + "." + program["extension"])
+
+    def createProgramBar(self):
         programButtons = []
-        #for program in config["programs"]:
-            #programButtons += ProgramButton(program, lambda: self.startProgram(program))
-        toolbar = QtWidgets.QToolBar("My ToolBar") #ProgramBar()
-        self.addToolBar(toolbar)
+        for key in self.pipelion.programs.keys():
+            program = self.pipelion.programs[key]
+            programButtons.append(ProgramButton(self.pipelion, program))
+        programBar = ProgramBar(self.pipelion, self, programButtons)
+        dockable = QtWidgets.QDockWidget("Programs", self)
+        container = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout()
+        for programButton in programButtons:
+            layout.addWidget(programButton)
+        container.setLayout(layout)
+        dockable.setWidget(container)
+        dockable.setFeatures(QtWidgets.QDockWidget.NoDockWidgetFeatures)
+        self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dockable)
 
     def createCheckedOutPane(self):
         items = QtWidgets.QDockWidget("Checked Out", self)
